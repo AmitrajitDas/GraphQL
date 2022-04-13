eb789732427941b6bc897d16d4f52ce0c20aeba1
AmitrajitDas <amitrajitdas31@gmail.com>
2022-04-14 01:37:45 +0530

    formal setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server')
+const { typeDefs } = require('./typeDefs')
+const { resolvers } = require('./resolvers/sample')
 
-const server = new ApolloServer()
+const server = new ApolloServer({ typeDefs, resolvers })
+
 server.listen().then(({ url }) => {
   console.log(`Server is ready at ${url}`)
 })
