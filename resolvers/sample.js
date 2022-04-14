c79c6d83c07082972cc4e57ba2b4bbb70491d8ae
AmitrajitDas <amitrajitdas31@gmail.com>
2022-04-14 14:23:15 +0530

    obj types

--- resolvers/sample.js
+++ resolvers/sample.js
@@ -5,6 +5,15 @@ const resolvers = {
     isCool: () => true,
     price: () => 69.25,
     assets: () => ['asset1', 'asset2'],
+    products: () => [
+      {
+        name: 'prod1',
+        description: 'desc1',
+        price: 28.63,
+        quantity: 12,
+        onSale: false,
+      },
+    ],
   },
 }
 
--- typeDefs.js
+++ typeDefs.js
@@ -1,12 +1,21 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
+  type Product {
+    name: String!
+    description: String!
+    price: Float!
+    quantity: Int!
+    onSale: Boolean!
+  }
+
   type Query {
     quote: String!
     age: Int!
     isCool: Boolean!
     price: Float!
     assets: [String!]!
+    products: [Product!]!
   }
 `
 
